c5bb0dc76c8bfef42afa774e5bd87a801b2c228c
tuxe88 <v.tourrilhes@gmail.com>
2017-03-20 23:31:58 -0300

    Agregadas pruebas unitarias

--- factores-primos/src/main/java/ar/edu/untref/aydoo/Program.java
+++ factores-primos/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -3,23 +3,34 @@ package ar.edu.untref.aydoo;
 public class Program{
     public static final void main(String arg[]){
 
-        int numero = Integer.parseInt(arg[0]);	
-	System.out.print("Factores primos de "+numero+": ");	
+    	int numero = Integer.parseInt(arg[0]);	
+	   System.out.print("Factores primos de "+numero+": ");	
 
-	while(numero!=1){
+		while(numero!=1){
 	    
-            int respuesta=2;
+	   	 int respuesta = getMinimoFactorPrimo(numero);
+		
+   	    System.out.print(respuesta+" ");
+       
+		    numero = numero/respuesta;	
+		} 
+
+		System.out.print("\n");	
+    }
+    
+    public static int getMinimoFactorPrimo(int numero){
+    
+    	if(numero==1) return numero;
+    	
+       int respuesta=2;
 	    
 	    while(numero%respuesta!=0){
 	        respuesta++;
 	    }
-		
-            System.out.print(respuesta+" ");
-            
 	
-	    numero = numero/respuesta;	
-	}
-
-	System.out.print("\n");	
+		return respuesta;
+    
     }
+    
+    
 }
