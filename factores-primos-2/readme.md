30cf53df2ae94e3dc93465c4dcb91123e58ef8bc
vtourrilhes <vtourrilhes@outlook.com>
2017-04-09 18:52:59 -0300

    Correciones hechas

---
+++ factores-primos-2/readme.md
@@ -0,0 +1,5 @@
+Correcciones:
+    -Se renombro el jar a factores-primos.jar
+    -Se corrigieron las llaves en Program.java
+    -Se eliminaron los espacios innecesacrios en el archivo Calculator.java
+  
--- factores-primos-2/src/main/java/ar/edu/untref/aydoo/Calculator.java
+++ factores-primos-2/src/main/java/ar/edu/untref/aydoo/Calculator.java
@@ -7,13 +7,10 @@ public class Calculator {
 
     public Integer[] calculatePrimeNumbers(int recieviedNumber) {
 
-
         if(recieviedNumber==1) return new Integer[]{1};
-
         List<Integer> numbersList = new ArrayList<>();
 
         while(recieviedNumber!=1){
-
             int answer = getLeastPrimeFactor(recieviedNumber);
             numbersList.add(answer);
             recieviedNumber = recieviedNumber/answer;
@@ -23,9 +20,6 @@ public class Calculator {
         arrayAnswer = numbersList.toArray(arrayAnswer);
         return arrayAnswer;
     }
-
-
-
 
     public static int getLeastPrimeFactor(int number){
 
