9d9a30a6ec8e6cfba666a0aae91485cab3b59319
tuxe88 <v.tourrilhes@gmail.com>
2017-05-14 19:17:11 -0300

    Se soluciono el problema del caso 1 y ya no se agrega .txt al archivo de salida

--- factores-primos-3/readme.md
+++ factores-primos-3/readme.md
@@ -1,11 +1,9 @@
+Factores primos 3
+=================
+
 Correcciones:
-    -Se solucionaron la mayoria de los errores por convenciones java
-    -Ahora la clase ArraySorter se ocupa de ordenar el array como se pide y la clase Formatter se ocupa de asignar el formato requerido
-    -La clase formatter ahora se ocupa de formatear el array a mostrar y de imprimir y crear el archivo si es que se pide
-    
-Dudas: 
-    -No entiendo porque el checkstyle me pide que los parametros a metodos sean final, pero le hice caso de todos modos
-    -No testee las excepciones de Formatter porque no se como generar esas excepciones en un test, y lo mismo
-     con la clase PrimeNumbers
-     -no me cierra mucho el manejo de archivos de la clase formatter, mas precisamente
-      el tema de los path 
+
+    - Se soluciono el problema del caso 1, ahora no muestra mas null y crea el archivo especificado
+
+    - Ya se no se agrega .txt al archivo de salida
+
--- factores-primos-3/src/main/java/ar/edu/untref/aydoo/PrimeNumbers.java
+++ factores-primos-3/src/main/java/ar/edu/untref/aydoo/PrimeNumbers.java
@@ -9,32 +9,36 @@ public class PrimeNumbers {
     private Integer[] sortedAnswer;
     private boolean writeFile;
     private String fileName;
+    private String sort;
 
     public PrimeNumbers() {
         this.calculator = new Calculator();
         this.formatter = new Formatter();
         this.arraySorter = new ArraySorter();
-
+        this.sort = "--sort=asc";
     }
 
     public void run(final String[] args) throws Exception {
 
         try {
-            //Recibo el numero a formatear
-            int recieviedNumber = Integer.parseInt(args[0]);
-            //Calculo el array de resultados
-            answer = calculator.calculatePrimeNumbers(recieviedNumber);
+
+            int receiviedNumber = Integer.parseInt(args[0]);
 
             //busco parametros opcionales en el array de argumentos
             for (int i = 0; i < args.length; i++) {
                 if (args[i].contains("--sort=")) {
-                   sortedAnswer =  this.arraySorter.sortArray(args[i], answer);
+                    this.sort = args[i];
                 }
-                if (args[i].contains("--output=")) {
-                    this.fileName = args[i].replace("--output=", "") + ".txt";
+                if (args[i].contains("--output-file=")) {
+                    this.fileName = args[i].replace("--output-file=", "");
                     this.writeFile =  true;
                 }
             }
+
+            //Calculo el array de resultados
+            answer = calculator.calculatePrimeNumbers(receiviedNumber);
+
+            sortedAnswer =  this.arraySorter.sortArray(sort, answer);
 
             String format = args[1];
 
@@ -43,10 +47,10 @@ public class PrimeNumbers {
                                                             format);
             if (this.writeFile) {
                 this.formatter.writeToFile(sortedAnswer, format,
-                        fileName, recieviedNumber);
+                        fileName, receiviedNumber);
             }
 
-            System.out.print("Factores primos " + recieviedNumber + ": ");
+            System.out.print("Factores primos " + receiviedNumber + ": ");
             System.out.print(toDisplay);
 
         } catch (Exception e) {
